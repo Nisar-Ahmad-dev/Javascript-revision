58a4748a527a563c8b62e02fcd7c2020e804ba09
Nisar Ahmad <165628092+Nisar-Ahmad-dev@users.noreply.github.com>
2025-05-19 12:22:40 +0000

    datatypes done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+let Username = "test"
+// let age = "19age"
+let age = null
+isLoginIN = true
+null
+undefined
+Object
+// console.log(typeof age);
+let agestring = Number(age) 
+
+console.log(typeof agestring);
+console.log(age);
+
+
+// console.log(typeof(undefined));
---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 12222;
+let accountEmail = "test@gmail.com";
+var accountName = "test";
+
+// accountId = 3333;
+// console.log(accountId);
+console.table([accountId, accountEmail, accountName]);
+
+// var is not work in scope   { code inside curly braces }
+// so used only let or const 
+
+
+
