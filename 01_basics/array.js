60d8f02422d9a1023bc88da9cd3769762c110023
Nisar Ahmad <165628092+Nisar-Ahmad-dev@users.noreply.github.com>
2025-05-21 12:18:27 +0000

    array done

---
+++ 01_basics/array.js
@@ -0,0 +1,17 @@
+/*
+1: First, An array can hold values of mixed types.For example
+numbers, string, boolean and null
+2: Second the size of array is dynamic and auto-growing.
+ In other we dont need to specify the size fo array 
+*/
+
+// Array useing  Array() constructor
+let test = new Array(1);
+test[0] = 1
+test[1] = 2
+test[2] = 3
+console.log(test);
+
+scores = [1 ,2 ,3 , test]
+ scores.push = 5
+console.log(scores);
