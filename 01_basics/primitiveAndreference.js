6beae020be6848fd280cb6b6ba2cd6402f4d2528
Nisar Ahmad <165628092+Nisar-Ahmad-dev@users.noreply.github.com>
2025-05-21 12:19:43 +0000

    primitive and reference done

---
+++ 01_basics/primitiveAndreference.js
@@ -0,0 +1,20 @@
+let age = 23
+
+let Newage= age
+
+let agechange = Newage + 1
+
+// console.table([age, Newage, agechange])
+
+
+let person = {
+    FirstName: 'Jhone',
+    LastName: 'smith'
+};
+
+let details = person;
+    details.LastName = 'test'
+console.log(person);
+console.log(details);
+
+
