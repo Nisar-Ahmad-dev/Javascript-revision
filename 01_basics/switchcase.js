fd2f90c95333e28f2e83302762068713744a1a72
Nisar Ahmad <165628092+Nisar-Ahmad-dev@users.noreply.github.com>
2025-05-22 12:45:05 +0000

    switchcase done

---
+++ 01_basics/switchcase.js
@@ -0,0 +1,19 @@
+let day = 1
+ let dayName
+
+ switch(day){
+
+    case 1:
+        dayName = "Mon"
+    break;
+    case 2:
+         dayName = "Thu"
+     break;
+     case 3:
+        dayName = 'we'
+        break;
+    default:
+        dayName = "NAN";
+ }
+
+ console.log(dayName);
