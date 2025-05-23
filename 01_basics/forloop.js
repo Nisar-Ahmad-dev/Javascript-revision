9b33c1e6450c9527ae45f1edad0d5747893e6e03
Nisar Ahmad <165628092+Nisar-Ahmad-dev@users.noreply.github.com>
2025-05-23 07:12:14 +0000

    for loop done

---
+++ 01_basics/forloop.js
@@ -0,0 +1,21 @@
+// for(let i = 10; i >= 0; i--){
+//     console.log(i);
+// }
+
+// for(j=1; j<10; j += 2){
+//     console.log(j);
+// }
+////for lop with out any expression statment
+// let j =1;
+// for(;;){
+//     if( j > 10){
+//         break;
+//     }
+//     console.log(j);
+//     j += 2
+// }
+
+// sum of value
+let sum = 0;
+  for(let i = 0; i <= 100; i++, sum += i);
+  console.log(sum);
