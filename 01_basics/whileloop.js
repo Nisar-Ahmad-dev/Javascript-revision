7ab0a465361d9168aad46dc7d1e76f038bd46398
Nisar Ahmad <165628092+Nisar-Ahmad-dev@users.noreply.github.com>
2025-05-23 10:15:12 +0000

    whileloop done

---
+++ 01_basics/whileloop.js
@@ -0,0 +1,63 @@
+//while loop executes a block of code as long as the condition is true
+
+// let count = 1;
+//   while(count < 10){ //first time value 1 ,3,5,7,9
+//     console.log(count);
+//     count += 2;
+//   }
+
+  // let total = 0;
+  // let i = 0;
+  // while(i <= 100)
+  // {
+  //     total += i;
+  //     i++;
+  // }
+  //  console.log(total);
+
+
+
+//   🧠 Task: Sum Even Numbers from 1 to 50
+// Write a JavaScript program using a while loop that:
+
+// Starts with i = 1
+
+// Loops until i reaches 50 (inclusive)
+
+// Adds only the even numbers to a total
+
+// Logs the final total outside the loop
+
+// let i = 1;
+// let total = 0;
+//    while(i <= 50){
+//     total += i;
+//     console.log(i);
+//     i +=2;
+//    }
+// console.log( 'sum of total value ', total);
+
+// let i = 2;
+// let total = 0;
+
+// while (i <= 50) {
+//     total += i;
+//     i += 2;
+// }
+
+// console.log('Sum of even numbers from 1 to 50 is:', total);
+
+
+// let row = 1
+//   while(row <= 5)
+//   {
+//     let stars = '';
+//     let count = 1;
+//     while(count <= row){
+//        stars += '*';
+//        count++;
+//     }
+//     console.log(stars);
+//     row++; 
+//   }
+  
