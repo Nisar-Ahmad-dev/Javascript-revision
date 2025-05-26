351b6ab88c225aa08845d51053395e153c93efd7
Nisar Ahmad <165628092+Nisar-Ahmad-dev@users.noreply.github.com>
2025-05-26 11:01:37 +0000

    simple function arguments and parameters  done

---
+++ 01_basics/function.js
@@ -0,0 +1,15 @@
+//use function for single parameters
+
+// function message(arrg) {
+//     return console.log(arrg);
+// }
+
+// message('Welcome To javascript Function')
+
+// use function for 2 parameters
+
+function add(x , y){
+    return console.log(x + y);
+}
+
+add(12,12);
