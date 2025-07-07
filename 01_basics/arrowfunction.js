10f71416ff0d2b3cbbeaed1f020fcaf0b6d9742d
Nisar Ahmad <165628092+Nisar-Ahmad-dev@users.noreply.github.com>
2025-07-07 12:31:00 +0000

    arrow function basic

---
+++ 01_basics/arrowfunction.js
@@ -0,0 +1,16 @@
+//function expression 
+
+
+function add( x, y){
+   let a = x;
+   let b = y;
+   return a+b;
+}
+console.log(add(10,30));
+
+
+let sum = (x ,y) => x + y;
+
+console.log(sum(30,40));
+
+
